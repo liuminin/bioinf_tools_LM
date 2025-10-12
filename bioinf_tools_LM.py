8f4084dcbc2ac7b59c72ed948c4013a54283cd3b
liuminin <lminina98@gmail.com>
2025-10-12 22:51:16 +0200

    entry: re-export file-based filter_fastq

--- bioinf_tools_LM.py
+++ bioinf_tools_LM.py
@@ -1,8 +1,8 @@
-
 """
+Main entry point for bioinformatics utilities.
+
 Here there are only imported public functions that users will call.
 """
 
-
+from modules.fastq import filter_fastq_file as filter_fastq
 from modules.run_dna_rna_tools import run_dna_rna_tools
-from modules.fastq import filter_fastq
